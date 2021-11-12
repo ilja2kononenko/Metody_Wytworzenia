fd4ca0376ee7a86b1958d5508375690ee2ad8ea3
elijah <256638@student.pwr.edu.pl>
2021-11-12 12:01:55 +0100

    init fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,10 @@
-module com.metody_wytworzenia {
+module com.example.demo {
     requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.metody_wytworzenia to javafx.fxml;
     exports com.metody_wytworzenia;
+    exports com.metody_wytworzenia.Controllers;
+    opens com.metody_wytworzenia.Controllers to javafx.fxml;
 }
